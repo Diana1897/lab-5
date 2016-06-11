3fa748c529a26937322e453293e077a93398017e
Diana1897 <diana.zikler@mail.ru>
2016-06-11 15:05:30 +0600

    End of lab5 and lab6

---
+++ test/test.c
@@ -0,0 +1,47 @@
+#include "ctest.h"
+#include "d.h"
+
+CTEST(test_suite, NO_ROOTS_test)
+{
+	const float a = 5, b = 4, c = 3;
+	float x1, x2;
+	const int expected_root_number = 0,
+		test = d(a, b, c, &x1, &x2);
+	ASSERT_EQUAL(expected_root_number, test);
+}
+
+CTEST(test_suite, ROOTS_NULL_test)
+{
+	const float a = 2, b = 4, c = 2;
+
+	const int test = d(a, b, c, NULL, NULL),
+		expected_root_number = -1;
+
+	ASSERT_EQUAL(expected_root_number, test);
+}
+
+CTEST(test_suite, ONE_ROOTS_test)
+{
+	const float a = 2, b = 4, c = 2,
+		expected_x1 = -4.000;
+	float x1, x2;
+	const int test = d(a, b, c, &x1, &x2),
+		expected_root_number = 1;
+
+	ASSERT_EQUAL(expected_root_number, test);
+	ASSERT_DBL_NEAR(expected_x1, x1);
+}
+
+CTEST(test_suite, TWO_ROOTS_test)
+{
+	const float a = 3, b = 7, c = 2,
+		expected_x1 = -2.000,
+		expected_x2 = -1.0/3.0;
+	float x1, x2;
+	const int test = d(a, b, c, &x1, &x2),
+		expected_root_number = 2;
+
+	ASSERT_EQUAL(expected_root_number, test);
+	ASSERT_DBL_NEAR(expected_x1, x1);
+	ASSERT_DBL_NEAR(expected_x2, x2);
+}
